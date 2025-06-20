437a598ade50128e63c7ee1c7a34d0af0af3c168
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-06-20 16:34:21 +0530

    Javascript

---
+++ Day-1/index.js
@@ -0,0 +1,95 @@
+// console.log(1);
+// console.log(2);
+// setTimeout(() => {
+//     console.log(3);
+    
+// }, 0);
+// console.log(4);
+
+/**
+ * 1
+ * 2
+ * 3
+ * 4
+ * -------
+ * 2
+ * 3
+ * 4
+ * 1
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+
+/**
+ * --Variables in Javascript--
+ * variables are containers,which are used to store the data,it perform special operations
+ * types of variables:-
+ *     1.var
+ *     2.let
+ *     3.const
+ *     4.automatic
+ * 
+ * syntax:-
+ *          variable variblename/identifier = value;
+ * 
+ */
+//var a=34;  //initilization
+// declaration
+       //  creating avariable with variableName or identifier without value
+    /**
+     * variable identifier;
+     */
+
+
+// assignment
+//assigning a value to the identifier which is already ready to use
+
+// identifier=value;
+
+/**
+ * identifiers are case-sencitive;
+ * keywords are not used as a identifiers
+ * identifiers are start with _ and $ also
+ * identifiers can not start with numbers
+ * 
+ */
+
+//
+// console.log(_Name);
+
+
+
+// a=10;
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
