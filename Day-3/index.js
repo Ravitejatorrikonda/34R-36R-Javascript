db042270e4e0ac76595ac0b8140cfe87bbabe657
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-06-23 17:41:56 +0530

    Day-3 Operatores

---
+++ Day-3/index.js
@@ -0,0 +1,76 @@
+/**
+ * operators
+ * in javascript,operators are special symbols and it perfomes specific actions on the values  variable
+ * operators are different types:-
+ * Arthematic operator
+ * Assignment Operators
+ * comparision operators
+ * logical opeeratores
+ * ternary operators
+ * 
+ * 
+ * Arthematic operatore
+ * mathamatical actoions
+ * + - / * % ++ --
+ * 
+ * assignment operator
+ * to assign a value to the variable
+ * 
+ * comparision operatore
+ * it compare the value and return boolean
+ * < > >= <= == === != !==
+ * 
+ * type conversion
+ * impicit 
+ * explacit
+ * string()
+ * boolean()
+ * number()
+ * 
+ * logical operatore
+ *&& || !
+
+
+ * true&&true // true
+ true&&false  //false
+ false&& true //false
+ false && false //false
+
+ true || true // true
+ true || false  //true
+ false || true //true
+ false || false //false
+
+ ternary operatore
+       ir is assigning a value to the variable based on condition
+
+       lat a=(condition)?expression 1: expression2;
+
+       bitwise
+ * 
+ */
+var a=+prompt("enter a number")
+ 
+let b=(a<=18)?"minore" : "majore";
+console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
