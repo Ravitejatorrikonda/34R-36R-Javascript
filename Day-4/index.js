2130d056544056eedece5428f4cf19ab1dbaf36f
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-06-24 17:39:04 +0530

    Conditional Statements

---
+++ Day-4/index.js
@@ -0,0 +1,83 @@
+/**
+ * Write a JavaScript script that uses the ternary operator to determine
+ if a number is even or odd.
+
+ * 
+ Grade Classification: Declare a variable score and 
+set it to a value between 0 and 100.
+ Use nested ternary operators to classify the score into grades:
+  A (>= 90), B (>= 75), C (>= 60), D (>= 45), and F (below 45).
+   Log the grade.
+
+ * conditional Statement:-
+   conditional statements are used to exicute block of block,
+
+   types of conditional statement:-
+   ->simple if
+         if(condition){
+           statements
+         }
+   ->if else
+      if(condition){
+      statements
+      }
+      else{
+
+      }
+
+   ->ladder if
+
+   if(condition){
+   statements
+   }
+   else if(condition){
+   statements
+   }
+   else{
+   statements
+   }
+ */
+
+      
+
+
+   var pavanLaptop=true;
+    if(pavanLaptop){
+        console.log("Zoom Meeting attended");
+        
+    }
+    else{
+    console.log("He is no attended Zoom");
+    }
+    console.clear();
+
+    // grade="B"   eligible
+    //marks=75;   eligible // not eligible
+
+
+    // age=13  child
+    // age=13 above and 19 below   teenger
+    // 19 above adult
+
+  
+    var age=19;
+
+    if(age<13){
+        console.log("Child");
+        
+    }
+    else if(age>=13 && age<=19 ){
+        console.log("Tenager");
+        
+    }
+    else{
+        console.log("adult");
+        
+    }
+
+
+
+
+    
+
+ 
