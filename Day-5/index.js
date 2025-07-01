0c8cbe5237d5bad81fc0a6ffc26e32ddeed5e901
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-07-01 17:46:33 +0530

    loops git commit -m loops
    clear
    git commit -m loops

---
+++ Day-5/index.js
@@ -0,0 +1,58 @@
+/**
+ * conditional statements:-
+ * 
+ * ->simple if
+ * ->if else
+ * ->ladder if
+ * ->nested if
+ * ->switch case
+ * 
+ * 
+ * marks 100 to 90 grade "A"
+ * 80 to 89 grade "B"
+ * 70 to 79 grade "c"
+ * 60 to 69 grade "d"
+ * 60 below fail
+ * 
+ * 
+ * if a persion is indian  gender checking
+ * 
+ * 
+ * switch(expression){
+ *   case value:
+ * //statements
+ *     break;
+ * }
+ * 
+ * default;
+ * //
+ * 
+ * 
+ * 
+ * 
+ * Loops in Javascript
+ * loops is ntg but itarative statements and also it exicute block of code until condition  if fail
+ * time saving
+ * less code
+ * 
+ * types of loops:-
+ * for loop
+ * while loop
+ * do while
+ * for off
+ * for in
+ * 
+ * 1.print 1-10 number and 10-1 without using < or >
+ * 2.print sum of 20 numbers
+ * 3.print sum of even number upto 50
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+
