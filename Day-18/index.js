e33d2a642fcba8fad80b54f575cf7cc6a2d52052
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-08-04 15:31:46 +0530

    Json GET POST

---
+++ Day-18/index.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * 
+ */
+// fetch("https://dummyjson.com/todos")
+// .then(response=>response.json())
+// .then(data=>{
+//     // console.log(data.todos)
+//     for(let i=0;i<=data.todos.length;i++){
+//         console.log(data.todos[i].userId
+// )
+//     }
+// })
+// GET POST Patch Delete PUT
+
+// let options={
+//     "method":"POST",
+//     "headers":{
+//         "Context-Text":"application/json"
+//     },
+//     "body":JSON.stringify({
+//         "id":4,
+//         "name":"Swamy"
+//     })
+
+
+    
+// }
+
+// fetch("http://localhost:3000/student",options)
+// .then(response=>{
+//     if(response.ok){
+//         console.log("data submitted")
+//     }else{
+//         console.log("data no entered");
+        
+//     }
+// })
+// .catch(data=>console.log("Error"))
