e943242641da05aa966b75a52d4e1f72dc87b7d5
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-08-19 15:17:24 +0530

    Array Methods

---
+++ Day-21/index.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * what is array:-
+ * array is an object,it is used to store a multiple value in a single variable
+ */
+// let arr=["html", "css", "Js"]
+
+// arr[0]="Boostrap"
+
+// console.log(arr)
+// let arr1 = [1, 2, "html", "css"]
+// let arr2=[1,2,3]
+// console.log(arr1.concat(arr2))
+// splice(startindex, deletecount,new1,new2,new3)
+// let arr=[1,2,3,4,5,6]
+// arr.pop()
+// arr.pop()
+// arr.pop()
+// arr.unshift("hello")
+
+// console.log(arr.unshift("there"))
+// let arr=["apple","banana","mango"]
+// console.log(arr.join(""))
+let arr=[1,2,3,4,5]
+arr.copyWithin(0,2)
+console.log(arr)
+
